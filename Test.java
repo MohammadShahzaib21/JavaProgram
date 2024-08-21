385f8fc4897b6146f6dfa6bacbbf9a1b66cc9f14
Mohammad Shahzaib <148314892+MohammadShahzaib21@users.noreply.github.com>
2024-08-21 16:09:09 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test
+  {
+    public static void main(String[] args)
+    {
+      void method()
+        {
+        System.out.println("This is the method of another class");
+      }
+    }
+  }
