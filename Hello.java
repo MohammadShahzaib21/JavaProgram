0d2ba130a64510b85a8e90ba2582925dd31bd340
Mohammad Shahzaib <148314892+MohammadShahzaib21@users.noreply.github.com>
2024-08-22 11:04:24 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ class Hello
     public static void main(String[] args)
     {
         System.out.println("Hello World How are you all");
-        new Test().method();
+        Test ob=new Test();
+        System.out.println(ob.method());
     }
 }
