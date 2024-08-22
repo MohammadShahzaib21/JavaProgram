845f570fc1480685dbdec425fbaed115ee9302f9
Mohammad Shahzaib <148314892+MohammadShahzaib21@users.noreply.github.com>
2024-08-22 11:24:41 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
 public class Test
   {
+    public static void main(String[] args)
+    {
+      System.out.println("Hello world");
+    }
      String method()
     {
       return "This function is from an another class";
